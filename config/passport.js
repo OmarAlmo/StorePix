725677de2d72d7d3bd82e73e4ce41c6883f2ff1f
OmarAlmo <omar.almoallim@gmail.com>
2019-06-09 16:33:01 -0400

    Verifies correct password (authentication corrected)

--- config/passport.js
+++ config/passport.js
@@ -23,15 +23,18 @@ module.exports = passport.use(new localStrategy({
                 });
             }
 
-            bcrypt.compare(password, user.password, function (err, res) {
+            bcrypt.compare(password, user.password, function (err, isMAtch) {
                 if (err) {
+                    throw err;
+                }
+                if (isMAtch) {
+                    return done(null, user);
+                } else {
                     return done(null, false, {
                         message: 'Password is incorrect'
                     });
                 }
             });
-
-            return done(null, user);
         })
     }
 ));
@@ -55,3 +58,61 @@ module.exports = {
         }
     }
 };
+
+module.exports = {
+    ensureAuthenticated: function (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        } else {
+            res.redirect('/users/login');
+        }
+    }
+};
+
+// module.exports = function (passport) {
+//     passport.use(new localStrategy({
+//         usernameField: 'email'
+//     }, (user, password, done) => {
+//         User.findOne({
+//             email: email
+//         }).then(user => {
+//             if (!user) {
+//                 return done(null, false, {
+//                     message: 'Email is not registered'
+//                 });
+//             } else {
+//                 bcrypt.compare(password, user.password, (err, isMatch) => {
+//                     if (err) {
+//                         throw err
+//                     }
+//                     if (isMatch) {
+//                         return done(null, user);
+//                     } else {
+//                         return done(null, false, {
+//                             message: 'Password is inccorect'
+//                         });
+//                     }
+//                 });
+//             }
+//         });
+//     }));
+//     passport.serializeUser(function (user, done) {
+//         done(null, user.id);
+//     });
+
+//     passport.deserializeUser(function (id, done) {
+//         User.findById(id, function (err, user) {
+//             done(err, user);
+//         });
+//     });
+// };
+
+// module.exports = {
+//     ensureAuthenticated: function (req, res, next) {
+//         if (req.isAuthenticated()) {
+//             return next();
+//         } else {
+//             res.redirect('/users/login');
+//         }
+//     }
+// };
